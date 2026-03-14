5742dfba6f48558795d9f5bdfece961abc808171
AzerUI1 <azer0808aslanov@gmail.com>
2026-03-14 22:18:33 +0400

    add solution of missing algorithm problem

---
+++ CSES/missing_num.cpp
@@ -0,0 +1,89 @@
+/** 
+    * @authr: Azer Aslanov;
+*/ 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FAST ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define vb vector<bool>
+#define vpi vector<pair<int, int>>
+#define vpll vector<pair<long long, long long>>
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a); i >= (b); i--)
+#define each(x, a) for (auto &x : a)
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+const int MOD = 1000000007;
+const int MOD2 = 998244353;
+const ll INF = 9e18;
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl
+#else
+#define debug(x)
+#endif
+
+ll power(ll a, ll b, ll mod = MOD) 
+{
+  ll r = 1;
+  while (b) {
+    if (b & 1)
+      r = (r * a) % mod;
+    a = (a * a) % mod;
+    b >>= 1;
+  }
+  return r;
+}
+
+ll gcdll(ll a, ll b) { return b == 0 ? a : gcdll(b, a % b); }
+ll lcmll(ll a, ll b) { return a / gcdll(a, b) * b; }
+
+void solve() 
+{
+	ll n, sum; 
+	cin >> n; 
+
+	ll a[n - 1]; 
+	
+	sum = 0; 
+	for (ll i = 0; i < n - 1; i++) 
+	{
+		cin >> a[i];
+	       	sum += a[i]; 	
+	} 
+	
+	ll sumN = (n * (n + 1)) / 2; 
+	ll ans = sumN - sum; 
+	
+	// cout << sumN << " " << sum << "\n"; 
+
+	cout << ans << "\n"; 
+
+	return; 
+}
+
+int main() 
+{
+  FAST;
+  
+  solve(); 
+
+  return 0;
+}
