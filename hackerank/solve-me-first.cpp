2020037bfb1cb12e2f565fe244d6366beee25c57
Azar Aslanov <azer0808aslanov@gmail.com>
2026-03-17 16:40:07 +0400

    Implement solve function to add two numbers

--- hackerank/solve-me-first.cpp
+++ hackerank/solve-me-first.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h> 
 
+#define ll long long 
+
+using namespace std;
+
+void solve() 
+{
+    ll a, b;
+    cin >> a >> b; 
+    
+    cout << a + b << "\n"; 
+}
+
+int main() 
+{
+    solve(); 
+    
+    return 0;
+}
