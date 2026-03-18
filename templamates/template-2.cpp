bca3bde1a79260879cd58b247694c513b4278cc4
Azar Aslanov <azer0808aslanov@gmail.com>
2026-03-19 00:04:46 +0400

    Create template-2.cpp

---
+++ templamates/template-2.cpp
@@ -0,0 +1,55 @@
+/*
+  Designed by printHelloWorld
+*/ 
+
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+using ll=long long;
+using ull=unsigned long long;
+
+#define ordered_set tree<pr, null_type,less<pr>, rb_tree_tag,tree_order_statistics_node_update>  
+#define $printHelloWorld$ ios_base::sync_with_stdio(false);cin.tie();cout.tie()
+#define endl "\n"
+#define for_i for(ll i=0; i<n; i++)
+#define for_j for(ll j=0; j<n; j++)
+#define for_s for(ll i=0; i<s.size(); i++)
+#define mp make_pair 
+#define pb push_back
+#define eb emplace_back
+#define pre pair<int, int>
+#define ff first
+#define ss second
+#define sorta sort(a,a+n) 
+#define sortv sort(v.begin(), v.end())
+#define sorts sort(s.begin(), s.end())
+#define mod 9929
+#define INF 1e18
+
+#ifdef LOCAL
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+void solve()
+{
+
+}
+
+int main()
+{  
+  $printHelloWorld$
+
+  ll t = 1; 
+  cin >> t; 
+
+  while (t--) 
+  {
+      solve(); 
+  }    
+
+  return (0); 
+}
