4045b1a2192e8c75d2c0c0fc6ee690aefe62d140
AzerUI1 <azer0808aslanov@gmail.com>
2026-03-18 23:28:18 +0400

    add file

---
+++ atcoder/abc449_c.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+
+#define ll long long
+#define sq(x) x * x
+
+using namespace std; 
+
+void solve()
+{
+	ll d; 
+	cin >> d; 
+	
+	cout << fixed << setprecision(20) << M_PI * d * d / 4 << endl;
+} 
+
+int main() 
+{
+	solve(); 
+
+	return (0); 
+} 
