bb56348597f646df28f37a1043dc84ad480e570e
AzerUI1 <azer0808aslanov@gmail.com>
2026-03-20 13:23:04 +0400

    add new file

---
+++ atcoder/abc439_a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+
+#define ll long long 
+
+using namespace std; 
+
+void solve() 
+{
+	ll n; 
+	cin >> n; 
+
+	cout << (1 << n) - 2 * n << "\n";
+} 
+
+int main() 
+{
+	solve(); 
+
+	return (0); 
+} 
