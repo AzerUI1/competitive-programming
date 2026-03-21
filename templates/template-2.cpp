086795a80ad693a2053c948f7183284b886b01f2
Azar Aslanov <azer0808aslanov@gmail.com>
2026-03-21 18:25:29 +0400

    Add template-2.cpp for Hello World program

---
+++ templates/template-2.cpp
@@ -0,0 +1,30 @@
+/* 
+    I Love printHelloWorld 
+*/
+
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef pair<ll, ll> pr;
+typedef tree<pr, null_type, less<pr>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define test_cases ll t; cin >> t; while (t--)
+#define printHelloWorld ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL); 
+
+const long long INF = 1e18;
+const long long MOD = 1e9 + 7;
+
+void solve();
+signed main(){printHelloWorld;solve();}
+
+void solve() 
+{
+
+}
